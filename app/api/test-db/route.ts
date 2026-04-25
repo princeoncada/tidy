4c24be605000b0f02b7b68a8364c5f75309f2607
princeoncada <prince.oncada@gmail.com>
2026-04-25 23:58:49 +0800

    chore(api): comment out user CRUD route handlers

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,53 +1,53 @@
-import { db } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
+// import { db } from "@/lib/db";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  let user = await db.user.create({
-    data: {
-      email: "sadf4@dfsf.com"
-    }
-  })
+// export async function POST(request: NextRequest) {
+//   let user = await db.user.create({
+//     data: {
+//       email: "sadf4@dfsf.com"
+//     }
+//   })
 
-  return NextResponse.json({ user })
-}
+//   return NextResponse.json({ user })
+// }
 
-export async function GET(request: NextRequest) {
-  let users = await db.user.findMany()
+// export async function GET(request: NextRequest) {
+//   let users = await db.user.findMany()
 
-  return NextResponse.json({ users })
-}
+//   return NextResponse.json({ users })
+// }
 
-export async function PUT(request: NextRequest) {
-  let res = await request.json()
+// export async function PUT(request: NextRequest) {
+//   let res = await request.json()
 
-  if (!res) {
-    return NextResponse.json({ data: "no user" })
-  }
+//   if (!res) {
+//     return NextResponse.json({ data: "no user" })
+//   }
 
-  let user = await db.user.update({
-    where: {
-      id: res.id
-    },
-    data: {
-      email: "cool@kid2.com"
-    }
-  })
+//   let user = await db.user.update({
+//     where: {
+//       id: res.id
+//     },
+//     data: {
+//       email: "cool@kid2.com"
+//     }
+//   })
 
-  return NextResponse.json({ data: user })
-}
+//   return NextResponse.json({ data: user })
+// }
 
-export async function DELETE(request: NextRequest) {
-  let res = await request.json()
+// export async function DELETE(request: NextRequest) {
+//   let res = await request.json()
 
-  if (!res) {
-    return NextResponse.json({ data: "no user" })
-  }
+//   if (!res) {
+//     return NextResponse.json({ data: "no user" })
+//   }
 
-  let user = await db.user.delete({
-    where: {
-      id: res.id
-    }
-  })
+//   let user = await db.user.delete({
+//     where: {
+//       id: res.id
+//     }
+//   })
 
-  return NextResponse.json({ data: user })
-}
+//   return NextResponse.json({ data: user })
+// }
