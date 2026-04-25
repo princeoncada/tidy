271a369d90b6dcb1350886d8174bb3e1f81447e8
princeoncada <prince.oncada@gmail.com>
2026-04-26 00:01:33 +0800

    refactor(dashboard): remove local user fetch from dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,32 +1,20 @@
 "use client";
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/client";
-import { User } from "@supabase/auth-js";
 import { Loader2 } from "lucide-react";
-import { useEffect, useState } from "react";
-import ListsContainer from "./list/ListsContainer";
-import UserAccountNav from "./UserAccountNav";
-import ListAdder from "./list/ListAdder";
-import { Separator } from "./ui/separator";
 import { redirect } from "next/navigation";
+import { useState } from "react";
+import ListAdder from "./list/ListAdder";
+import ListsContainer from "./list/ListsContainer";
+import { Separator } from "./ui/separator";
+import UserAccountNav from "./UserAccountNav";
 
 const supabase = createClient();
 
 const Dashboard = () => {
 
   const [loggingOut, setLoggingOut] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  async function getUser() {
-    const currUser = await supabase.auth.getUser();
-    setUser(currUser.data.user);
-  }
 
   function handleLogout() {
     setLoggingOut(true);
@@ -47,16 +35,16 @@ const Dashboard = () => {
           <div className="flex flex-col gap-2.5 w-full items-center">
             <div className="w-full flex flex-col">
               <div className="w-full flex justify-between items-end h-12">
-                <div className="flex gap-3">
-                  <UserAccountNav email={user?.email} name={user?.user_metadata.name} logout={handleLogout} />
-                  <h1 className="text-3xl md:text-[40px] font-bold text-gray-900">
+                <div className="flex gap-3 items-end">
+                  <UserAccountNav logout={handleLogout} />
+                  <h1 className="text-[28px] md:text-[32px] font-bold text-gray-900">
                     Your Todo Lists
                   </h1>
                 </div>
                 <ListAdder />
               </div>
             </div>
-            <Separator className="bg-zinc-200/30" />
+            <Separator className="bg-zinc-200 md:bg-zinc-200/30" />
           </div>
           <ListsContainer />
         </div>
