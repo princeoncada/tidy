0fdddf540ceb9f0203f9e2e347b4b09a1ed3d471
princeoncada <prince.oncada@gmail.com>
2026-04-26 15:44:29 +0800

    feat(list): implement ListMenu dropdown with add and delete actions

--- components/list/ListMenu.tsx
+++ components/list/ListMenu.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { Button } from '../ui/button';
+import { Ellipsis } from 'lucide-react';
 
-const ListMenu = () => {
-  return (
-    <div>ListMenu</div>
-  )
+
+interface ListMenuProps {
+  handleViewListItemAdder: () => void;
+  handleDeleteList: () => void;
 }
 
-export default ListMenu
+
+const ListMenu = ({
+  handleViewListItemAdder,
+  handleDeleteList
+}: ListMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+      >
+        <Button size="icon-lg" variant="ghost" aria-label="List options">
+          <Ellipsis />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          variant="default"
+          className='hover:cursor-pointer'
+          onSelect={handleViewListItemAdder}
+        >
+          Add Item
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          variant="default"
+          className='hover:cursor-pointer'
+        >
+          Export
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          variant='destructive'
+          className='hover:cursor-pointer'
+          onSelect={handleDeleteList}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ListMenu;
