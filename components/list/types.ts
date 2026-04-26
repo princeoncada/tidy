b828d9c7dd4533d0f7004e95584cc599aa0e3f98
princeoncada <prince.oncada@gmail.com>
2026-04-26 15:47:21 +0800

    feat(types): add optimistic flags for list and list items

--- components/list/types.ts
+++ components/list/types.ts
@@ -4,3 +4,9 @@ export type Lists = RouterOutputs['getListsWithItems']
 export type List = Lists[number]
 export type ListItems = List['listItems']
 export type ListItem = ListItems[number]
+export type OptimisticList = List & {
+  isOptimistic?: boolean;
+};
+export type OptimisticListItem = ListItem & {
+  isOptimistic?: boolean;
+};
