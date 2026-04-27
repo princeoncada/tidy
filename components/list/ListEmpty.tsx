47f89ae9ff2ae7159aaeebe34844f178a9921001
princeoncada <prince.oncada@gmail.com>
2026-04-27 16:12:10 -0400

    feat(empty-state): add list empty state with CTA

---
+++ components/list/ListEmpty.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '../ui/empty';
+import { Button } from '../ui/button';
+import { FolderOpen, Plus } from 'lucide-react';
+import ListAdder from './ListAdder';
+
+const ListEmpty = () => {
+  return (
+    <Empty className='mt-12'>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <FolderOpen className='text-zinc-400' />
+        </EmptyMedia>
+        <EmptyTitle>No Lists Yet</EmptyTitle>
+        <EmptyDescription>
+          You haven&apos;t created any lists yet. Get started by creating
+          your to-do list.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <ListAdder />
+      </EmptyContent>
+    </Empty>
+  );
+};
+
+export default ListEmpty;
