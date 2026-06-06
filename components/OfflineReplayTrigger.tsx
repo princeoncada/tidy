7e6f98702308388650379c23560508f5a5441ca1
princeoncada <prince.oncada@gmail.com>
2026-06-06 16:39:57 -0400

    feat(sync): add offline replay trigger mount component

---
+++ components/OfflineReplayTrigger.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useOfflineReplayTrigger } from "@/hooks/use-offline-replay-trigger";
+
+export function OfflineReplayTrigger() {
+  useOfflineReplayTrigger();
+  return null;
+}
