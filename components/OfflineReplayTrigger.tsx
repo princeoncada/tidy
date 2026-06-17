314a15adfba5956798e2f0c2ffc259fd3354e495
princeoncada <prince.oncada@gmail.com>
2026-06-17 09:49:43 -0400

    refactor(write): retire outbox write-capture/replay path

--- components/OfflineReplayTrigger.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useOfflineReplayTrigger } from "@/hooks/use-offline-replay-trigger";
-
-export function OfflineReplayTrigger() {
-  useOfflineReplayTrigger();
-  return null;
-}
